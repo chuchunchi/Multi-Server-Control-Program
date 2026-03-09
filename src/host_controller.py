fb5dff9906f79bc93242241aceedfa05c8f9e4c3
chuchunchi <chuchun1231@gmail.com>
2026-03-09 23:59:26 +0800

    fix: use logger to fix concurrent print error

--- src/host_controller.py
+++ src/host_controller.py
@@ -1,5 +1,9 @@
 import paramiko
 from src.config import config_dict
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def connect_to_host(host):
     ssh = paramiko.SSHClient()
@@ -9,14 +13,15 @@ def connect_to_host(host):
 
 def execute_command(ssh, command):
     _, stdout, stderr = ssh.exec_command(command)
-    print(stdout.read().decode("utf-8"))
-    print(stderr.read().decode("utf-8"))
+    logger.info(" stdout: " + stdout.read().decode("utf-8"))
+    logger.error(" stderr: " + stderr.read().decode("utf-8"))
     return
 
 def connect_and_execute_commands(host, commands):
     ssh = connect_to_host(host)
     try:
         for command in commands:
+            logger.info(f"Execute command on {host}: {command['name']}")
             execute_command(ssh, command["bash"])
     finally:
         ssh.close()
